c53dc8f41154133662d393bbf822e05aa368c1d9
Phillip <mehblahwhatever@gmail.com>
2017-11-28 20:49:11 -0500

    variables tut

---
+++ cplusplus_com/02_variables.cpp
@@ -0,0 +1,22 @@
+// variables
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	// declaring variables
+	int a, b;
+	int result;
+
+	// process
+	a = 5;
+	b = 2;
+	a = a + 1;
+	result = a - b;
+
+	// print result
+	cout << result;
+
+	// terminate
+	return 0;
+}
